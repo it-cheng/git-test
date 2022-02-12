ca84bfe03757bf46368fab13ad0b9112e7a03f3e
clw <itcheng@163.com>
2022-02-12 19:32:36 +0800

    aaa

--- src/main/java/cn/cn/itcheng/GitTest.java
+++ src/main/java/cn/cn/itcheng/GitTest.java
@@ -7,5 +7,6 @@ package cn.cn.itcheng;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
