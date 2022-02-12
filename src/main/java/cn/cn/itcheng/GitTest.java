60d6ba89fbcc0ba3b56c25ee90b2b62d6787bde2
clw <itcheng@163.com>
2022-02-12 19:30:30 +0800

    aaa

---
+++ src/main/java/cn/cn/itcheng/GitTest.java
@@ -0,0 +1,11 @@
+package cn.cn.itcheng;
+
+/**
+ * @author 程立文
+ * @create 2022-02-12 19:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
